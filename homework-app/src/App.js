5b37bb598c74e85585286b38c86cb00c43d802cd
parisapahlevan <paris.pahlevan@gmail.com>
2019-06-12 11:10:12 -0400

    completed app.js

--- homework-app/src/App.js
+++ homework-app/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import CardContainer from "./components/CardContainer"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // setting the state
+  state = {
+    count: 0,
+    topScore: 0
+  }
+
+  // update the current score count in the nav
+  updateCurrentScore = (newCount) => {
+    // set the new count as the count
+    this.setState({count: newCount});
+  }
+
+  // update the top score in the nav
+  updateTopScore = (newTop) => {
+    // if the new top score is higher than the current
+    if (newTop > this.state.topScore) {
+      // then set the state but you have to subtract 1
+      this.setState({topScore: newTop - 1})
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar score={this.state.count} top={this.state.topScore}/>
+        <Header/>
+        <CardContainer updateCurrentScore={this.updateCurrentScore} updateTopScore={this.updateTopScore}/>
+      </div>
+    );
+  }
 }
 
 export default App;
