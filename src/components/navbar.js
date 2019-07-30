c6f718b39b57ebe5145f6f9a717308207ad56bd7
Parisa Pahlevan <parisapahlevan@Parisas-MacBook-Pro.local>
2019-07-30 14:32:24 -0400

    more changes on design

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 class Navbar extends Component {
     render(){
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+            <nav id="navi" className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
                 <a className="navbar-brand" href="/" id="logo"><h3>Clicky Game</h3></a>
                 <h5 className="ml-auto mt-2" id="counters">Score: {this.props.score} | Top score: {this.props.top} </h5>
             </nav>
